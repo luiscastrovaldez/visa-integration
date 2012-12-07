ccf9b654d6c086e9b9f51d0e9358efd8b997f816
taniadgv@gmail.com <taniadgv@gmail.com@c8f520ad-d5cf-c7d0-f988-4b733d2587fa>
2012-12-07 00:42:31 +0000

    Login styles

--- trunk/VisaIntegration/src/main/java/com/visa/util/VisaIntegrationConstants.java
+++ trunk/VisaIntegration/src/main/java/com/visa/util/VisaIntegrationConstants.java
@@ -10,4 +10,18 @@ public class VisaIntegrationConstants {
 	public static final String MSG_ERROR_GENERICO = "Ha ocurrido un error, por favor intente más tarde.";
 	public static final String CAMPO_XML_SERVICE = "xmlService";
 
+	public static final String TIPO_USUARIO_ALUMNO = "0";
+	public static final String TIPO_USUARIO_POSTULANTE = "1";
+	public static final String TIPO_USUARIO_PROSPECTO = "2";
+
+	public static final String LOGIN_USUARIO_ALUMNO = "Nombre de Usuario:";
+	public static final String LOGIN_USUARIO_POSTULANTE = "Código de Postulante:";
+	public static final String LOGIN_USUARIO_PROSPECTO = "Email:";
+
+	public static final String LOGIN_CLAVE_ALUMNO = "Constraseña:";
+	public static final String LOGIN_CLAVE_POSTULANTE = "Nro. Identificación:";
+	public static final String LOGIN_CLAVE_PROSPECTO = "Nro. Identificación:";
+
+	public static final String APP_TITULO = "Sistema de Pago Electrónico";
+
 }
