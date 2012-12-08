470ff013585c3b4c0561ae083f851af3429e55f1
taniadgv@gmail.com <taniadgv@gmail.com@c8f520ad-d5cf-c7d0-f988-4b733d2587fa>
2012-12-08 03:33:06 +0000

    Adding service property to paymentbean

--- trunk/VisaIntegration/src/main/java/com/visa/beans/PaymentBean.java
+++ trunk/VisaIntegration/src/main/java/com/visa/beans/PaymentBean.java
@@ -7,81 +7,80 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 import com.visa.domain.Carrera;
 import com.visa.services.VisaIntegration;
 import com.visa.util.VisaIntegrationConstants;
 
-
-@ManagedBean(name="paymentBean")
+@ManagedBean(name = "paymentBean")
 @SessionScoped
-public class PaymentBean implements Serializable{
+public class PaymentBean implements Serializable {
 
-	private String tipoUsuarioTexto;
-	private List<Carrera> listaCarreras;
-	private Carrera carrera;
-	
-	@ManagedProperty(value="#{userManagedBean}")
-	private UserManagedBean userManagedBean;
-	
-	
-	
-	public UserManagedBean getUserManagedBean() {
-		return userManagedBean;
-	}
+  private String tipoUsuarioTexto;
+  private List<Carrera> listaCarreras;
+  private Carrera carrera;
 
-	public void setUserManagedBean(UserManagedBean userManagedBean) {
-		this.userManagedBean = userManagedBean;
-	}
+  @ManagedProperty(value = "#{userManagedBean}")
+  private UserManagedBean userManagedBean;
 
-	@Autowired
-	VisaIntegration visaIntegration;
+  @ManagedProperty(value = "#{visaIntegration}")
+  VisaIntegration visaIntegration;
 
-	public PaymentBean() {
-		listaCarreras = new ArrayList<Carrera>();
-		
-		// TODO get user type
-		tipoUsuarioTexto = VisaIntegrationConstants.CODIGO_USUARIO_TEXTO
-				+ VisaIntegrationConstants.USUARIO_ALUMNO;
-	}
+  public UserManagedBean getUserManagedBean() {
+    return userManagedBean;
+  }
 
-	public String getTipoUsuarioTexto() {
-		return tipoUsuarioTexto;
-	}
+  public void setUserManagedBean(UserManagedBean userManagedBean) {
+    this.userManagedBean = userManagedBean;
+  }
 
-	public void setTipoUsuarioTexto(final String tipoUsuarioTexto) {
-		this.tipoUsuarioTexto = tipoUsuarioTexto;
-	}
+  public VisaIntegration getVisaIntegration() {
+    return visaIntegration;
+  }
 
-	public List<Carrera> getListaCarreras() {
-		listaCarreras = new ArrayList<Carrera>();
-		System.out.println("userManagedBean = " + userManagedBean);
-		System.out.println(" username = " + userManagedBean.getUsername());
-		System.out.println(" clave = " + userManagedBean.getPassword());
-		try {
-			listaCarreras = visaIntegration.obtenerCarrerasPostgrado("CL20031506");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+  public void setVisaIntegration(VisaIntegration visaIntegration) {
+    this.visaIntegration = visaIntegration;
+  }
 
-		return listaCarreras;
-	}
+  public PaymentBean() {
+    listaCarreras = new ArrayList<Carrera>();
 
-	public void setListaCarreras(List<Carrera> listaCarreras) {
-		this.listaCarreras = listaCarreras;
-	}
+    // TODO get user type
+    tipoUsuarioTexto = VisaIntegrationConstants.CODIGO_USUARIO_TEXTO + VisaIntegrationConstants.USUARIO_ALUMNO;
+  }
 
-	public Carrera getCarrera() {
-		return carrera;
-	}
+  public String getTipoUsuarioTexto() {
+    return tipoUsuarioTexto;
+  }
 
-	public void setCarrera(Carrera carrera) {
-		this.carrera = carrera;
-	}
+  public void setTipoUsuarioTexto(final String tipoUsuarioTexto) {
+    this.tipoUsuarioTexto = tipoUsuarioTexto;
+  }
+
+  public List<Carrera> getListaCarreras() {
+    listaCarreras = new ArrayList<Carrera>();
+    System.out.println("userManagedBean = " + userManagedBean);
+    System.out.println(" username = " + userManagedBean.getUsername());
+    System.out.println(" clave = " + userManagedBean.getPassword());
+    try {
+      listaCarreras = visaIntegration.obtenerCarrerasPostgrado("CL20031506");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return listaCarreras;
+  }
+
+  public void setListaCarreras(List<Carrera> listaCarreras) {
+    this.listaCarreras = listaCarreras;
+  }
+
+  public Carrera getCarrera() {
+    return carrera;
+  }
+
+  public void setCarrera(Carrera carrera) {
+    this.carrera = carrera;
+  }
 
 }
--- trunk/VisaIntegration/src/main/java/com/visa/beans/UserManagedBean.java
+++ trunk/VisaIntegration/src/main/java/com/visa/beans/UserManagedBean.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Collection;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -13,172 +12,167 @@ import javax.servlet.http.HttpServletRequest;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
-import com.visa.domain.LogTransaction;
 import com.visa.services.VisaIntegration;
 import com.visa.util.VisaIntegrationConstants;
 
-//@Component
 @ManagedBean(name="userManagedBean")
 @SessionScoped
 public class UserManagedBean implements Serializable{
 
-	UserService userService = new UserService();
+  UserService userService = new UserService();
 
-	@Autowired
-	VisaIntegration visaIntegration;
+  @Autowired
+  VisaIntegration visaIntegration;
 
-	public void setVisaIntegration(VisaIntegration visaIntegration) {
-		this.visaIntegration = visaIntegration;
-	}
+  public void setVisaIntegration(VisaIntegration visaIntegration) {
+    this.visaIntegration = visaIntegration;
+  }
 
-	private String username;
-	private String password;
-	private String searchUser;
-	private Collection<User> searchUsersResults;
-	private User selectedUser;
+  private String username;
+  private String password;
+  private String searchUser;
+  private Collection<User> searchUsersResults;
+  private User selectedUser;
 
-	private String firstname;
-	private String surname;
+  private String firstname;
+  private String surname;
 
-	public String getFirstname() {
-		return firstname;
-	}
+  public String getFirstname() {
+    return firstname;
+  }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
 
-	public String getSurname() {
-		return surname;
-	}
+  public String getSurname() {
+    return surname;
+  }
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
 
-	public void savePerson() {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Welcome " + firstname + " " + surname + "!"));
-	}
+  public void savePerson() {
+    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Welcome " + firstname + " " + surname + "!"));
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public String getUsername() {
+    return username;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-	public User getSelectedUser() {
-		if (selectedUser == null) {
-			selectedUser = new User();
-		}
-		return selectedUser;
-	}
+  public User getSelectedUser() {
+    if (selectedUser == null) {
+      selectedUser = new User();
+    }
+    return selectedUser;
+  }
 
-	public void setSelectedUser(User selectedUser) {
-		this.selectedUser = selectedUser;
-	}
+  public void setSelectedUser(User selectedUser) {
+    this.selectedUser = selectedUser;
+  }
 
-	public Collection<User> getSearchUsersResults() {
-		return searchUsersResults;
-	}
+  public Collection<User> getSearchUsersResults() {
+    return searchUsersResults;
+  }
 
-	public void setSearchUsersResults(Collection<User> searchUsersResults) {
-		this.searchUsersResults = searchUsersResults;
-	}
+  public void setSearchUsersResults(Collection<User> searchUsersResults) {
+    this.searchUsersResults = searchUsersResults;
+  }
 
-	public String getSearchUser() {
-		return searchUser;
-	}
+  public String getSearchUser() {
+    return searchUser;
+  }
 
-	public void setSearchUser(String searchUser) {
-		this.searchUser = searchUser;
-	}
+  public void setSearchUser(String searchUser) {
+    this.searchUser = searchUser;
+  }
 
-	public String login() {
-		System.out.println("user " + getUsername());
+  public String login() {
+    System.out.println("user " + getUsername());
 
-		/*LogTransaction logTransaction = visaIntegration.findById(1L);
-		if (logTransaction != null) {
-			System.out.println(" id = " + logTransaction.getId());
-		}*/
+    /*LogTransaction logTransaction = visaIntegration.findById(1L);
+    if (logTransaction != null) {
+      System.out.println(" id = " + logTransaction.getId());
+    }*/
 
-		if ("test".equalsIgnoreCase(getUsername()) && "test".equals(getPassword())) {
-			return "pagos";
-		} else {
-			FacesContext context = FacesContext.getCurrentInstance();
-			context.addMessage("username", new FacesMessage("Invalid UserName and Password"));
-			return "login";
-		}
-	}
+    if ("test".equalsIgnoreCase(getUsername()) && "test".equals(getPassword())) {
+      return "pagos";
+    } else {
+      FacesContext context = FacesContext.getCurrentInstance();
+      context.addMessage("username", new FacesMessage("Invalid UserName and Password"));
+      return "login";
+    }
+  }
 
-	public String searchUser() {
-		String username = (this.searchUser == null) ? "" : this.searchUser.trim();
-		this.searchUsersResults = userService.searchUsers(username);
-		System.out.println(searchUsersResults);
-		return "home";
-	}
+  public String searchUser() {
+    String username = (this.searchUser == null) ? "" : this.searchUser.trim();
+    this.searchUsersResults = userService.searchUsers(username);
+    System.out.println(searchUsersResults);
+    return "home";
+  }
 
-	public String updateUser() {
-		userService.update(this.selectedUser);
-		return "home";
-	}
+  public String updateUser() {
+    userService.update(this.selectedUser);
+    return "home";
+  }
 
-	public void onUserSelect(SelectEvent event) {
-		selectedUser = (User) event.getObject();
-		System.out.println("selectedUser = " + selectedUser);
-	}
+  public void onUserSelect(SelectEvent event) {
+    selectedUser = (User) event.getObject();
+    System.out.println("selectedUser = " + selectedUser);
+  }
 
-	public void onUserUnselect(UnselectEvent event) {
-		selectedUser = null;
-	}
+  public void onUserUnselect(UnselectEvent event) {
+    selectedUser = null;
+  }
 
-	public void cerrarSesion() {
-	}
+  public void cerrarSesion() {
+  }
 
-	public String getUserNameLabelText() {
-		final String strTipoUsuario = getTipoUsuario();
-		if (VisaIntegrationConstants.TIPO_USUARIO_ALUMNO.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_USUARIO_ALUMNO;
-		} else if (VisaIntegrationConstants.TIPO_USUARIO_POSTULANTE.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_USUARIO_POSTULANTE;
-		} else if (VisaIntegrationConstants.TIPO_USUARIO_PROSPECTO.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_USUARIO_PROSPECTO;
-		}
-		return null;
-	}
+  public String getUserNameLabelText() {
+    final String strTipoUsuario = getTipoUsuario();
+    if (VisaIntegrationConstants.TIPO_USUARIO_ALUMNO.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_USUARIO_ALUMNO;
+    } else if (VisaIntegrationConstants.TIPO_USUARIO_POSTULANTE.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_USUARIO_POSTULANTE;
+    } else if (VisaIntegrationConstants.TIPO_USUARIO_PROSPECTO.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_USUARIO_PROSPECTO;
+    }
+    return null;
+  }
 
-	public String getPasswordLabelText() {
-		final String strTipoUsuario = getTipoUsuario();
-		if (VisaIntegrationConstants.TIPO_USUARIO_ALUMNO.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_CLAVE_ALUMNO;
-		} else if (VisaIntegrationConstants.TIPO_USUARIO_POSTULANTE.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_CLAVE_POSTULANTE;
-		} else if (VisaIntegrationConstants.TIPO_USUARIO_PROSPECTO.equals(strTipoUsuario)) {
-			return VisaIntegrationConstants.LOGIN_CLAVE_PROSPECTO;
-		}
-		return null;
-	}
+  public String getPasswordLabelText() {
+    final String strTipoUsuario = getTipoUsuario();
+    if (VisaIntegrationConstants.TIPO_USUARIO_ALUMNO.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_CLAVE_ALUMNO;
+    } else if (VisaIntegrationConstants.TIPO_USUARIO_POSTULANTE.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_CLAVE_POSTULANTE;
+    } else if (VisaIntegrationConstants.TIPO_USUARIO_PROSPECTO.equals(strTipoUsuario)) {
+      return VisaIntegrationConstants.LOGIN_CLAVE_PROSPECTO;
+    }
+    return null;
+  }
 
-	public boolean getUsuarioLogueado() {
-		return getUsername() != null;
-	}
+  public boolean getUsuarioLogueado() {
+    return getUsername() != null;
+  }
 
-	private String getTipoUsuario() {
-		final HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-		return request.getParameter("usr") == null ? VisaIntegrationConstants.TIPO_USUARIO_ALUMNO : request.getParameter("usr");
-	}
+  private String getTipoUsuario() {
+    final HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+    return request.getParameter("usr") == null ? VisaIntegrationConstants.TIPO_USUARIO_ALUMNO : request.getParameter("usr");
+  }
 
 }
